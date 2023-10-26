e488fa21738dd6a979884fda85882240e48bc2e5
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-10-26 15:49:01 +0600

    26/10/23

--- DS_1501_CT.py
+++ DS_1501_CT.py
@@ -10,3 +10,30 @@ motorcycles.reverse()
 print(motorcycles[2][1])
 motorcycles[2].remove('honda')
 
+#10/25/2023 CT
+#Qus-3:
+list = []
+print(f"Enter your numbers: (Enter 'f' when you are done.)")
+choices = 0
+while choices != 'f':
+    choice = input()
+    list.append(choice)
+    choices = choice
+else:
+    list.pop()
+    print(list)
+if len(list) == 1:
+    print("Balda diso amar")
+else:
+    odd = 0
+    even = 0
+    for x in list:
+        indx = list.index(x)
+        if indx%2 == 0 and indx>0:
+            even += int(x)
+        elif indx == 0:
+            even+= int(x)
+        else:
+            odd += int(x)
+    print(f"Sum of odd index:{odd}")
+    print(f"Sum of even index:{even}")
