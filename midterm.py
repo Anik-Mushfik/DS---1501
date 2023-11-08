85ba459bc453a223f1765f3fc12851ee12138570
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-11-09 02:45:23 +0600

    Update midterm.py

--- midterm.py
+++ midterm.py
@@ -1,4 +1,4 @@
-#1-(c)
+#1-(c)-
 str = "Data Science, United International University."
 listofwords = []
 word = ""
@@ -16,3 +16,42 @@ word = ""
 for w in listofwords:
     word = word+w[0]
 print(word)
+
+
+#2(a)-
+listA = [4,6,-3,-4,2,6]
+sum = 0
+for i in listA:
+    if i < 0:
+        sum += i
+print(sum)
+
+
+#2(b)-
+listCountries = ["Bangladesh", "India", "Pakistan", "Srilanka", "Afganistan"]
+listcapitals = ["Dhaka", "Delhi", "Islamad", "Colombo", "Kabul"]
+user_input = input()
+if user_input in listCountries:
+    i = listCountries.index(user_input)
+    print(listcapitals[i])
+else:
+    print("Unknown")
+
+
+#2(c)-
+listNum = [4,5,-7,41,3,17,2]
+maxi = listNum[0]
+for i in listNum:#Vul = range(1,len(listNum)):
+    if i > maxi:
+        maxi = i
+print(maxi)
+
+
+#3(c)-
+message = "I love watching cricket, but quality our cricket should be improved."
+i = message.find("cricket")
+if i != -1:
+    message = message.replace("cricket", "soccer")
+    print(message)
+
+
