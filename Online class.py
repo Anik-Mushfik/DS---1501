7701b531242e215c70049048fea7c4f216d4e725
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-11-12 15:07:54 +0600

    Create Online class.py

---
+++ Online class.py
@@ -0,0 +1,99 @@
+#We pass argument or parameter to a function
+#Python functions - print(),input(),max(),min(),sum(),len(),int(),float(),type()
+
+
+#Make your own function-
+
+# First define your function
+def function_name():
+    # inside the function
+    print("I am a function")
+
+# Call the functiuon-
+function_name() #signature of the function(functioner namer pashe parentheses)
+
+
+# Parameter of a funcion-
+def paraFunc(X): # x is a parameter
+    print(X)
+
+paraFunc("Bangladesh will win World Cup") # this string is called argument
+
+
+#** Returning value-
+def multFunc(x):
+    return x*2
+val = multFunc(20)
+print(val)
+
+
+# Multiple parameter-
+def multiFunc(x,y):
+    if x>y:
+        return x
+    else:
+        return y
+print(multiFunc(14,5))
+
+def powerof(num,pow):
+    return num**pow
+print(powerof(pow=2,num=5)) #Keyword argument
+
+# Practice 1:
+def word(x,y):
+    if x>y:
+        return y
+    else:
+        return x
+print(word("apple","bat"))
+
+
+# Default value-
+def poerof(num,pow=1):
+    return num**pow
+print(poerof(10,2))
+print(poerof(10))
+
+
+# List as a parameter-
+
+
+# Practice 2:
+def lists(L1,L2):
+    L1sum = sum(L1)
+    L2sum = sum(L2)
+    if L1sum > L2sum:
+        return L1sum - L2sum
+    else:
+        return L2sum - L1sum
+print(lists([12,12,13],[40,50,60]))
+
+def lists(L1,L2):
+    L1sum = sum(L1)
+    L2sum = sum(L2)
+    x = L1sum - L2sum
+    if x>0:
+        return x
+    else:
+        return -x
+print(lists([12,12,13],[40,50,60]))
+
+
+
+# Local variables-
+
+x = 20 # Global variable
+
+def muFunc(s): # s is also a local variable
+    n = 50 # n is a Local variable(Shudhu function er modhe chinbe)
+# Local k shudhu functioner vitore chine but Global k vitore baire shobkhane chine.
+print(x)
+
+
+x = 20
+def funcX():
+    global x # Global variable k function er vitore chancge kora jaye
+    x = 10
+
+print(funcX())
+print(x)
