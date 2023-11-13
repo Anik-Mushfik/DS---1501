a0784a4ef23aca7e47673eee0601d9d4929326dd
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-11-14 01:44:18 +0600

    Update Function.py

--- Self Study/Function.py
+++ Self Study/Function.py
@@ -1,5 +1,11 @@
 #Ex - 1:
 def greet_user():
     """Display a simple greeting."""
-    print("Hello World")
+    print("Hello World!")
 greet_user()
+
+def greet_user(username):
+    """Display a simple greeting with a name."""
+    print(f"Hello, {username}!")
+greet_user("Musfique")
+
