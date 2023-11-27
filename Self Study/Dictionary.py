1cc5a07146c827c7733cb2d24159552b1af7f6b6
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-11-28 00:04:10 +0600

    Update Dictionary.py

--- Self Study/Dictionary.py
+++ Self Study/Dictionary.py
@@ -327,6 +327,53 @@ for person in people:
         print(f"Thank you {person.title()} for responding.")
     else:
         print(f"Hi {person.title()}! I have noticed that you didn't participated in the poll yet. Can you please respond fast?")
+
+
+# aliens.py -
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(alien)
+
+aliens = []
+
+for alien_number in range(30):
+    new_alien  = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[:5]:
+    print(alien)
+
+print(f"Number of aliens: {len(aliens)}")
+
+
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['points'] = 10
+        alien['speed'] = 'medium'
+
+for alien in aliens[:5]:
+    print(alien)
+print()
+
+for alien in aliens[:7]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['points'] = 10
+        alien['speed'] = 'medium'
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['points'] = 15
+        alien['speed'] = 'fast'
+
+for alien in aliens[:10]:
+    print(alien)
+
 """
 
 
