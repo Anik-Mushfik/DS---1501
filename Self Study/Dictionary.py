7ae505db632a46240601f59f02b66e812a064663
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-12-01 11:43:26 +0600

    Update Dictionary.py

--- Self Study/Dictionary.py
+++ Self Study/Dictionary.py
@@ -665,4 +665,13 @@ for x in snigle:
     if x not in double:
         unique.append(x)
 print(unique)
+
+
+# Print all value once in a set:
+L = [{"V":"S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII":"S005"}, {"V":"S009"},{"VIII":"S007"}]
+u_value = set(val for dic in L for val in dic.values()) #set comprihension
+print(u_value)
+v_value = {val for dic in L for val in dic.values()}
+print(v_value)
+
 """
