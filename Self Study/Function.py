114e91703be353c4e6e9a9b7d0b9ede214b16c4f
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-12-07 18:08:28 +0600

    Update Function.py

--- Self Study/Function.py
+++ Self Study/Function.py
@@ -39,3 +39,13 @@ describe_pet("cat", "hogi") # Order matters
 describe_pet("hamster", 'sala')
 describe_pet(animal_type='dog', pet_name='kutta') # Order doesn't matter for keyword arguments.
 describe_pet(pet_name='sagol', animal_type="cow")
+
+def describe_pet(pet_name, animal_type= 'dog'):
+    print(f"I have a {animal_type}")
+    print(f"My {animal_type}'s nami is {pet_name}")
+
+describe_pet('kutta')
+describe_pet(pet_name='kutta')
+describe_pet('sagol', 'cow')
+describe_pet('cow', 'sagol') # Ye galat haye
+describe_pet(animal_type='cow', pet_name='sagol') # Ye sahi haye
