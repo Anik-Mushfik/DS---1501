e4958f22c8eae27629a8b8a77197796ea773f754
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-12-07 16:55:36 +0600

    Update Function.py

--- Self Study/Function.py
+++ Self Study/Function.py
@@ -1,12 +1,15 @@
 ## Ex - 1:
+# greeter.py
 def greet_user():
     """Display a simple greeting."""
     print("Hello World!")
-greet_user()
 
+greet_user()
+# greeter.py
 def greet_user(username):
     """Display a simple greeting with a name."""
     print(f"Hello, {username}!")
+
 greet_user("Musfique")
 
 
@@ -26,3 +29,13 @@ favourite_book("Alice in Wonderland")
 
 
 ## Ex - 2:
+# pets.py
+def describe_pet(animal_type, pet_name):
+    """Describe information about a pet"""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name}.")
+
+describe_pet("cat", "hogi") # Order matters
+describe_pet("hamster", 'sala')
+describe_pet(animal_type='dog', pet_name='kutta') # Order doesn't matter for keyword arguments.
+describe_pet(pet_name='sagol', animal_type="cow")
