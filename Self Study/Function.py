7bd3b1bf811bb7dfe3734d4a8232e8ca894c6655
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-12-08 00:23:05 +0600

    Update Function.py

--- Self Study/Function.py
+++ Self Study/Function.py
@@ -122,3 +122,25 @@ name = formatted_name('mushfi', 'ahmed')
 print(name.title())
 
 
+# Ex-4:
+# person.py
+def build_person(first_name, last_name):
+    """Return a dictionary of information about a person."""
+    person = {'first': first_name, 'last': last_name}
+    return person
+
+info = build_person('musfique', 'ahmed')
+print(info)
+
+def build_person(first_name, last_name, age= None):
+    """Return a dictionary of information about a person."""
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+
+info = build_person('musfique', 'ahmed', 27)
+print(info)
+info = build_person('musfique', 'ahmed', age= 27)
+print(info)
+
