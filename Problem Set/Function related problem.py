7951c3bc6185cde3e276a5ba1e5c084e8b957e71
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-12-16 16:38:55 +0600

    Update Function related problem.py

--- Problem Set/Function related problem.py
+++ Problem Set/Function related problem.py
@@ -548,16 +548,58 @@ def ShowMatrix(mat):
 
 def InputMatrix():
     """Take inupt for the matrix from the user"""
-    user_input = input().split()
-    lst = [int(x) for x in user_input]
-    matrix.append(lst)
+    for i in range(3):
+        user_input = input().split()
+        lst = [int(x) for x in user_input]
+        matrix.append(lst)
     
 matrix = []
 
-row_num = int(input("Enter the amount of rows: "))
-for i in range(row_num):
-    InputMatrix()
-num = int(input("Input the number you want to mutiply the matrix with: "))
+first_call = InputMatrix()
+
+num = int(input("\n"))
+
+diplay = ShowMatrix(matrix)
+multiply = ScalarMultiply(matri=matrix, n=num)
+
+
+# Problem - 19:
+def ScalarMultiply(matri, n):
+    """Multiply the matrix with the inputed number."""
+    print(f"\nMultiplied by {n}:")
+    for i in matri:
+        for j in i:
+            x = j*n
+            print(x, end="\t")
+        print()
+
+def ShowMatrix(mat):
+    """Disply the original matrix"""
+    print(f"\nOriginal:")
+    for i in mat:
+        for j in i:
+            print(j, end="\t")
+        print()
+
+def InputMatrix(row_num, column_num):
+    """Take inupt for the matrix from the user"""
+    print(f"\nEnter the matrix:")
+    for i in range(row_num):
+        user_input = input().split()
+        lst = [int(x) for x in user_input]
+        if len(lst) != column_num:
+            print("Input Error! Rerun the program!!!")
+        matrix.append(lst)
+    
+matrix = []
+lst = list(map(int, input("Enter the dimention of the matrix: (Row Column)\n").split()))
+
+row = lst[0]
+column = lst[1]
+
+first_call = InputMatrix(row_num=row, column_num=column)
+
+num = int(input("\nMultiply by:\n"))
 
 diplay = ShowMatrix(matrix)
 multiply = ScalarMultiply(matri=matrix, n=num)
