e85fa42bbb7c01657c7e8fe9a3b020ca60887b79
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2023-12-27 23:59:12 +0600

    Update file_reader.py

--- Self Study/Files/Book/file_reader.py
+++ Self Study/Files/Book/file_reader.py
@@ -3,3 +3,10 @@ data = file.read()
 data = data.rstrip('\n')
 print(data)
 file.close()
+
+file = open('D:\Python Study\DS---1501\Self Study\Files\Book\pi_digits.txt', 'r')
+data = file.readlines()
+print(data)
+
+for line in data:
+    print(line.strip(), end="")
