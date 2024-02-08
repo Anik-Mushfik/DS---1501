35abd0f094a3cd61a013a09dcf211abaaaca3f8b
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2024-02-08 07:03:12 +0600

    Update final.py

--- Exam/final.py
+++ Exam/final.py
@@ -32,3 +32,9 @@ print(*max_neigh, sep="\n=>")
 print(f"Upozillas with minimum neighbours: \n=>", end="")
 print(*min_neigh, sep="\n=>")
 
+## Answer: 2
+# (a):
+def evenSum(x,y):
+    """Find the even numbers in the range of (x-y) 
+    and return the sum of those even numbers."""
+        
